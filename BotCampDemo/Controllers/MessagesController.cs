9eff919f60d3820122831094311d69a62758f8a4
won liao <won.liao@taiwantaxi.com.tw>
2017-04-22 13:51:35 +0800

    test

--- BotCampDemo/Controllers/MessagesController.cs
+++ BotCampDemo/Controllers/MessagesController.cs
@@ -169,7 +169,7 @@ namespace BotCampDemo
 						template_type = "airline_checkin",
 						intro_message = "你最近的一筆預約",
 						locale =  "zh_TW",
-						pnr_number = "車輛編號：AA-1234",
+						pnr_number = "AA-1234",
 						//theme_color = "#FFFF00",
 						flight_info = new List<object>()
 						{
