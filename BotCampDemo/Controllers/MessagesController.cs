8b23281b7ac810721c4346b6314b28982b01721e
won liao <won.liao@taiwantaxi.com.tw>
2017-04-23 12:47:41 +0800

    test

--- BotCampDemo/Controllers/MessagesController.cs
+++ BotCampDemo/Controllers/MessagesController.cs
@@ -217,20 +217,16 @@ namespace BotCampDemo
                         {
                             new
                             {
-                                title = "iPad Pro",
-                                image_url = "https://s.yimg.com/wb/images/936392DB6B69D9C6D1B897B8DAB20AE595E96FA4",
-                                buttons = new List<object>()
-                                {
-                                    new
-                                    {
-										type = "web_url",
-										url = "https://zh.wikipedia.org/wiki/Wiki",
-										title = "Show Website"
-                                    }
-                                }
+								template_type = "web_url",
+								url = "https://zh.wikipedia.org/wiki/Wiki",
+								title = "Select Criteria",
+								webview_height_ratio = "full",
+								messenger_extensions = true,
+								fallback_url = "https://petersfancyapparel.com/fallback"
                             }
                         }
-               
+
+
 					}
 				}
 			});
